72f93abf0b6cdb2834b7e90f76a40c7b18344b32
aysse.ortega <aysse.ortega@accenture.com>
2020-04-15 10:52:44 +0100

    Testando Jenkins

--- triangle.py
+++ triangle.py
@@ -2,6 +2,7 @@ import incompleteTriangle
 import hypotenuse
 
 def getTriangle(a, b):
+    # Testing Jenkins
     initTriangle = incompleteTriangle.Incompletetriangle(a, b)
     hypo = hypotenuse.Hypotenuse(initTriangle)
     return round(hypo.hypotenuse)
